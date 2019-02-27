3b27f13789e076b348cfb3c85864331248ed1310
Adonaï Elohim Exaucé <adonainangui03@gmail.com>
2019-02-27 14:09:16 +0000

    Apply fixes from StyleCI (#1)

--- tests/JokeFactoryTest.php
+++ tests/JokeFactoryTest.php
@@ -2,38 +2,38 @@
 
 namespace Nangui\ChuckNorrisJokes\Test;
 
+use PHPUnit\Framework\TestCase;
 use Nangui\ChuckNorrisJokes\JokeFactory;
-use \PHPUnit\Framework\TestCase;
 
 class JokeFactoryTest extends TestCase
 {
-	/** @test */
-	public function it_returns_a_random_joke()
-	{
-		$jokes = new JokeFactory([
-			'This is a joke'
-		]);
+    /** @test */
+    public function it_returns_a_random_joke()
+    {
+        $jokes = new JokeFactory([
+            'This is a joke',
+        ]);
 
-		$joke = $jokes->getRandomJoke();
+        $joke = $jokes->getRandomJoke();
 
-		$this->assertSame('This is a joke', $joke);
-	}
+        $this->assertSame('This is a joke', $joke);
+    }
 
-	/** @test */
-	public function it_returns_a_predefined_joke()
-	{
-		$chuckNorrisJokes = [
-			'The First rule of Chuck Norris is: you do not talk about Chuck Norris.',
-			'Chuck Norris does not wear a condom. Because there is no such thing as protection from Chuck Norris.',
-			'Chuck Norris counted to infinity... Twice.',
-			'The chief export of Chuck Norris is Pain.',
-			'Chuck Norris uses pepper spray to spice up his steaks.'
-		];
+    /** @test */
+    public function it_returns_a_predefined_joke()
+    {
+        $chuckNorrisJokes = [
+            'The First rule of Chuck Norris is: you do not talk about Chuck Norris.',
+            'Chuck Norris does not wear a condom. Because there is no such thing as protection from Chuck Norris.',
+            'Chuck Norris counted to infinity... Twice.',
+            'The chief export of Chuck Norris is Pain.',
+            'Chuck Norris uses pepper spray to spice up his steaks.',
+        ];
 
-		$jokes = new JokeFactory();
+        $jokes = new JokeFactory();
 
-		$joke = $jokes->getRandomJoke();
+        $joke = $jokes->getRandomJoke();
 
-		$this->assertContains($joke, $chuckNorrisJokes);
-	}
+        $this->assertContains($joke, $chuckNorrisJokes);
+    }
 }
